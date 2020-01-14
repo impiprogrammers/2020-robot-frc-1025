90f72d798d73298fa6f267dd73f46052e6163f62
impiprogrammers <impiprogrammers@gmail.com>
2020-01-14 17:41:36 -0500

    changed motor controller constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,6 @@ package frc.robot;
 public final class Constants {
     public static final int XBOX_CONTROLLER = 0;
 
-    public static final int SHOOTER_LEFT_MOTOR = 0;
-    public static final int SHOOTER_RIGHT_MOTOR = 1;
+    public static final int SHOOTER_LEFT_MOTOR = 1;
+    public static final int SHOOTER_RIGHT_MOTOR = 3;
 }
