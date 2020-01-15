73d156f9e0a7467167b74ceefe9cad3e99747305
impiprogrammers <impiprogrammers@gmail.com>
2020-01-15 18:53:26 -0500

    deleted ExampleSubsystem and ExampleCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.ShooterShoot;
 import frc.robot.commands.ShooterStop;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
