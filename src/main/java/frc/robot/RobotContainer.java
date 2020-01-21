eb440728d8d1082011e8dbfff41171c78852ed5d
impiprogrammers <impiprogrammers@gmail.com>
2020-01-21 15:53:33 -0500

    create base code branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,25 +26,10 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 
 	// Subsystems
-	public static final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-	public static final ChassisSubsystem chassisSubsystem = new ChassisSubsystem();
 	
 	// Commands
-	private final ShooterShoot shooterShoot1 = new ShooterShoot(4000);
-	private final ShooterShoot shooterShoot2 = new ShooterShoot(5000);
-	private final ShooterShoot shooterShoot3 = new ShooterShoot(5700);
-	private final ShooterStop shooterStop = new ShooterStop();
-
-	private final ChassisDrive chassisDrive = new ChassisDrive();
 
  	// OI
-	public static final XboxController driverController = new XboxController(Constants.XBOX_CONTROLLER_DRIVER);
-	public static final XboxController buttonsController = new XboxController(Constants.XBOX_CONTROLLER_BUTTONS);
-	
-	private final JoystickButton buttonA = new JoystickButton(buttonsController, XboxController.Button.kA.value);
-	private final JoystickButton buttonB = new JoystickButton(buttonsController, XboxController.Button.kB.value);
-	private final JoystickButton buttonX = new JoystickButton(buttonsController, XboxController.Button.kX.value);
-	private final JoystickButton buttonY = new JoystickButton(buttonsController, XboxController.Button.kY.value);
 
  	public RobotContainer() {
 		configureButtonBindings();
@@ -57,10 +42,6 @@ public class RobotContainer {
   	* {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
   	*/
  	private void configureButtonBindings() {
-		buttonA.whenPressed(shooterShoot1);
-		buttonX.whenPressed(shooterShoot2);
-		buttonY.whenPressed(shooterShoot3);
-		buttonB.whenPressed(shooterStop);
 	  }
 
  	/**
