99057ccc54f283d76f2f11d62c6db7b1d125c5b1
impiprogrammers <impiprogrammers@gmail.com>
2020-01-25 09:51:47 -0500

    deleted stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,13 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.ShooterFeederSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ChasisSubsystem;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -30,13 +25,7 @@ import frc.robot.ImpiLib;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
-  public static ChasisSubsystem chasisSubsystem = new ChasisSubsystem();
-  public static IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   public static ShooterFeederSubsystem shooterFeederSubsystem = new ShooterFeederSubsystem();
 
 
