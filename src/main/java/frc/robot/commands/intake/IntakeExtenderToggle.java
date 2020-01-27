e9d3b1ab0aefb27ac6ed11644bfb63a240ac4995
impiprogrammers <impiprogrammers@gmail.com>
2020-01-27 17:09:59 -0500

    Intake Naming Fix

--- src/main/java/frc/robot/commands/intake/IntakeExtenderToggle.java
+++ src/main/java/frc/robot/commands/intake/IntakeExtenderToggle.java
@@ -26,7 +26,7 @@ public class IntakeExtenderToggle extends CommandBase {
 	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {
-		intakeSubsystem.extenderToggle();
+		intakeSubsystem.intakeToggle();
 	}
 
 	// Called once the command ends or is interrupted.
