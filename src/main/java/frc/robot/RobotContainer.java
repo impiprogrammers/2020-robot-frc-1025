ab67ed5b87448d0c72e9931651f6734f75b63192
impiprogrammers <impiprogrammers@gmail.com>
2020-01-31 15:54:49 -0500

    deleted unused subsstems/commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,11 +13,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.*;
 import frc.robot.commands.chassis.*;
-import frc.robot.commands.climber.*;
-import frc.robot.commands.conveyor.*;
-import frc.robot.commands.intake.*;
-import frc.robot.commands.shooter.*;
-import frc.robot.commands.shooter_feeder.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -30,32 +25,9 @@ public class RobotContainer {
 
 	// Subsystems
 	public static final ChassisSubsystem chassisSubsystem = new ChassisSubsystem();
-	public static final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-	public static final PIDShooterSubsystem shooterSubsystem = new PIDShooterSubsystem();
-	public static final ConveyorSubsystem conveyorSubsystem = new ConveyorSubsystem();
-	public static final ShooterFeederSubsystem shooterFeederSubsystem = new ShooterFeederSubsystem();
-	public static final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
 
 	// Commands
 	private final ChassisDrive chassisDrive = new ChassisDrive(chassisSubsystem);
-
-	private final IntakeExtenderToggle intakeExtenderToggle = new IntakeExtenderToggle();
-	private final IntakeRollersRoll intakeRollersRoll = new IntakeRollersRoll();
-
-	private final ShooterShoot shooterShoot = new ShooterShoot(5700);
-	private final ShooterStop shooterStop = new ShooterStop();
-	private final ShooterToggle shooterToggle = new ShooterToggle(5700);
-
-	private final ConveyorRoll conveyorRoll = new ConveyorRoll();
-
-	private ShooterFeederSpin shooterFeederSpin = new ShooterFeederSpin();
-	private ShooterFeederStop shooterFeederStop = new ShooterFeederStop();
-	
-	private final ClimberExtenderExtend climberExtenderExtend = new ClimberExtenderExtend();
-	private final ClimberExtenderRetract climberExtenderRetract = new ClimberExtenderRetract();
-	private final ClimberLockToggle climberLockToggle = new ClimberLockToggle();
-	private final ClimberWinchMove climberWinchMove = new ClimberWinchMove();
-	private final ClimberShimmyMove climberShimmyMove = new ClimberShimmyMove();
 
 	// OI
 	public static final XboxController driverController = new XboxController(Constants.XBOX_CONTROLLER_DRIVER);
