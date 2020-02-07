20c84ac96ca603d3bfac10c6f9a6d56e9b05b8e1
Shewell <shewejff@schaeffler.com>
2020-02-07 07:20:41 -0500

    Clean up of constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,65 +1,33 @@
-
-/*----------------------------------------------------------------------------*/
-/*01* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                      */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-    // OI
-    public static final int XBOX_CONTROLLER_DRIVER = 0;
-    public static final int XBOX_CONTROLLER_BUTTONS = 1;
 
-    // Chassis
-    public static final int CHASSIS_LEFT_FRONT_PORT = 0;
-	public static final int CHASSIS_RIGHT_FRONT_PORT = 1;
-	public static final int CHASSIS_LEFT_REAR_PORT = 2;
-    public static final int CHASSIS_RIGHT_REAR_PORT = 3;
-    
-    // Intake
-    public static final int INTAKE_EXTENDER_MODULE = 4;
-    public static final int INTAKE_ROLLERS_PORT = 5;
+    public final class OI {
+        public static final int DRIVER = 0;
+        public static final int BUTTONS = 1;
+    }
 
-    // Turret
-    public static final int TURRET_ROTATE_PORT = 6;
-    
-    // Shooter
-    public static final int SHOOTER_LEFT_PORT = 7;
-    public static final int SHOOTER_RIGHT_PORT = 8;
-    
-    // Conveyor
-    public static final int CONVEYOR_ROLLERS_PORT = 11;
+    public final class CAN {
+        public static final int CHASSIS_DRIVE_MOTOR_LEFT_FRONT = 0;
+        public static final int CHASSIS_DRIVE_MOTOR_LEFT_REAR = 1;
+        public static final int CHASSIS_DRIVE_MOTOR_RIGHT_FRONT = 2;
+        public static final int CHASSIS_DRIVE_MOTOR_RIGHT_REAR = 3;
+        public static final int SHOOTER_MOTOR_LEFT = 7;
+        public static final int SHOOTER_MOTOR_RIGHT = 8;
+        public static final int CONVEYOR_ROLLERS_MOTOR = 11;
+        public static final int INTAKE_ROLLERS_MOTOR = 5;
+        public static final int SHOOTER_FEEDER_MOTOR = 12;
+        public static final int TURRET_MOTOR = 6;
+        public static final int CLIMBER_WINCH_MOTOR = 15;
+        public static final int CLMBER_SHIMMY_MOTOR = 16;
+        public static final int CONTROL_PANEL_WHEEL_MOTOR = 18;
+    }
 
-    // Shooter Feeder
-    public static final int SHOOTER_FEEDER_PORT = 12;
-
-    // Climber
-    public static final int CLIMBER_EXTENDER_MODULE = 13;
-    public static final int CLIMBER_LOCK_MODULE = 14;
-    public static final int CLIMBER_WINCH_PORT = 15;
-    public static final int CLMBER_climberShimmy_PORT = 16;
-
-    // Control Panel
-	public static final int CONTROL_PANEL_WHEEL_PORT = 18;
-    public static final int CONTROL_PANEL_PISTON_MODULE = 19;
-
-    // PCM
-    public static final int INTAKE_EXTENDER_CHANNEL = 0;
-    
-    public static final int CLIMBER_EXTENDER_CHANNEL = 1;
-    public static final int CLIMBER_LOCK_CHANNEL = 2;
-
-    public static final int CONTROL_PANEL_PISTON_CHANNEL = 3;
+    public final class PCM {
+        public static final int INTAKE_ARM = 4;
+        public static final int CLIMBER_ARM_EXTEND = 13;
+        public static final int CLIMBER_ARM_RETRACT = 13;
+        public static final int CLIMBER_LOCK = 14;
+        public static final int CONTROL_PANEL_ARM = 19;
+    }
 }
-
